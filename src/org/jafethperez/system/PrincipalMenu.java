30f1f3f9ed2278eff8706fab067da83654b3b85c
Jafeth Pérez <jperez-2026078@kinal.edu.gt>
2026-05-29 00:23:52 -0600

    001 Se agregaron unas clase y se inicio con la clase system/PrincipalMenu

---
+++ src/org/jafethperez/system/PrincipalMenu.java
@@ -0,0 +1,16 @@
+
+package org.jafethperez.system;
+import org.jafethperez.view.MenuPrincipall;
+/**
+ *
+ * @author jafeth pérez
+ */
+public class PrincipalMenu {
+
+    public static void main(String[] args) {
+    System.out.println("Hola mundo!");
+        MenuPrincipall menu = new MenuPrincipall();
+        menu.arrancarSistema();
+    }
+    
+}
