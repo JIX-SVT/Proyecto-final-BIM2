535860bb2d7851a55d1b8a08ecbac7a548dbe683
Jafeth Pérez <jperez-2026078@kinal.edu.gt>
2026-05-29 01:10:06 -0600

    003 Se van agregando el menu de habitaciones y el momento de mostrar la tabla

--- src/org/jafethperez/view/HabitacionesView.java
+++ src/org/jafethperez/view/HabitacionesView.java
@@ -1,10 +1,36 @@
-
 package org.jafethperez.view;
 
-/**
- *
- * @author savit
- */
+import java.util.List;
+import java.util.Scanner;
+import org.jafethperez.model.Habitaciones;
+
 public class HabitacionesView {
-    
+    private final Scanner scanner = new Scanner(System.in);
+
+    public int mostrarMenu() {
+        System.out.println("\n--- GESTION DE HABITACIONES ---");
+        System.out.println("1. Listar todas las habitaciones");
+        System.out.println("2. Regresar al Menu Principal");
+        System.out.print("Seleccione una opcion: ");
+        return scanner.nextInt();
+    }
+
+    public void desplegarLista(List<Habitaciones> habitaciones) {
+        System.out.println("\n=========================== LISTADO DE HABITACIONES ===========================");
+        System.out.printf("%-15s %-10s %-20s %-15s\n", "NO. HABITACION", "PISO", "PRECIO POR NOCHE", "TIPO");
+        System.out.println("-------------------------------------------------------------------------------");
+        
+        for (Habitaciones hab : habitaciones) {
+            System.out.printf("%-15d %-10d Q%-19.2f %-15s\n", 
+                hab.getNumeroDeHabitacion(), 
+                hab.getPisoHabitacion(), 
+                hab.getPrecioNocheHabitacion(), 
+                hab.getTipoHabitacion());
+        }
+        System.out.println("===============================================================================");
+    }
+
+    public void mostrarMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
 }
