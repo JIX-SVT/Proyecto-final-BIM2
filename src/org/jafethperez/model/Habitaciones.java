09d1260d6b3c270892fcf2b69bf56f666bc80f42
Jafeth Pérez <jperez-2026078@kinal.edu.gt>
2026-05-29 01:40:43 -0600

    005 Se crearon los constructores a la tabla

--- src/org/jafethperez/model/Habitaciones.java
+++ src/org/jafethperez/model/Habitaciones.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.jafethperez.model;
 
-/**
- *
- * @author savit
- */
 public class Habitaciones {
+    private int numeroDeHabitacion;
+    private int pisoHabitacion;
+    private double precioNocheHabitacion;
+    private String tipoHabitacion;
     
+    public Habitaciones() {
+    }
+
+    public Habitaciones(int numeroDeHabitacion, int pisoHabitacion, double precioNocheHabitacion, String tipoHabitacion) {
+        this.numeroDeHabitacion = numeroDeHabitacion;
+        this.pisoHabitacion = pisoHabitacion;
+        this.precioNocheHabitacion = precioNocheHabitacion;
+        this.tipoHabitacion = tipoHabitacion;
+    }
+
+    // Getters y Setters 
+    public int getNumeroDeHabitacion() {
+        return numeroDeHabitacion;
+    }
+
+    public void setNumeroDeHabitacion(int numeroDeHabitacion) {
+        this.numeroDeHabitacion = numeroDeHabitacion;
+    }
+
+    public int getPisoHabitacion() {
+        return pisoHabitacion;
+    }
+
+    public void setPisoHabitacion(int pisoHabitacion) {
+        this.pisoHabitacion = pisoHabitacion;
+    }
+
+    public double getPrecioNocheHabitacion() {
+        return precioNocheHabitacion;
+    }
+
+    public void setPrecioNocheHabitacion(double precioNocheHabitacion) {
+        this.precioNocheHabitacion = precioNocheHabitacion;
+    }
+
+    public String getTipoHabitacion() {
+        return tipoHabitacion;
+    }
+
+    public void setTipoHabitacion(String tipoHabitacion) {
+        this.tipoHabitacion = tipoHabitacion;
+    }
 }
